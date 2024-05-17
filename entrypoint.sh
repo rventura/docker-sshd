21bc5a0707ba28cc66778878923d998783b3e211
Rodrigo Ventura <rodrigo.ventura@isr.tecnico.ulisboa.pt>
2024-05-17 07:07:37 +0100

    Remove prior user with given UID

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 if [ -z "${USER+x}" ]; then USER="user" ;fi
 if [ -z "${PASS+x}" ]; then PASS="xpto" ;fi
 if [ -z "${UID+x}" ];  then UID=999 ;fi
+userdel "$(id -nu ${UID})"
 useradd -d /files -M -p $(openssl passwd -1 "${PASS}") -u ${UID} "${USER}"
 service ssh start
 sleep infinity

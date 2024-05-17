3a97a6a21895164bed01bb84aaa4c09c522944e6
Rodrigo Ventura <rodrigo.ventura@isr.tecnico.ulisboa.pt>
2024-05-17 07:42:05 +0100

    Fix to add group of GID

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ if [ -z "${PASS+x}" ]; then PASS="xpto" ;fi
 if [ -z "${UID+x}" ];  then UID=1001 ;fi
 if [ -z "${GID+x}" ];  then GID=1001 ;fi
 userdel "$(id -nu ${UID})"
+groupadd -g ${GID} "${USER}"
 useradd -d /files -M -p $(openssl passwd -1 "${PASS}") -u ${UID} -g ${GID} "${USER}"
 service ssh start
 sleep infinity
